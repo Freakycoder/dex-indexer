6a915d3dfc51acba332dd084925ddfc53e163a26
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-11 21:54:23 +0400

    feat: added public mod

---
+++ backend/shared/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod redis;
+pub mod types;
+pub mod ys_grpc;
+
+const RADUIM_AMM_V4 : &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+const RADUIM_CLMM : &str = "CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK";
