db1665158330cc5404ac8506535d368c3be02a3d
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-13 01:10:21 +0400

    feat : init logic implemented

---
+++ backend/shared/src/websocket/ws_manager.rs
@@ -0,0 +1,13 @@
+struct WebsocketManager{
+    websocket_url : String
+}
+
+impl WebsocketManager {
+    fn new(ws_url : String) -> Self{
+        Self { websocket_url: ws_url }
+    }
+
+    fn initialize_websocket(){
+        
+    }
+}
