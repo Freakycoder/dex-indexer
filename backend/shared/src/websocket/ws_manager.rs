94c50cbe4b400a6d1001a2990d195ebf396e9ae4
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-13 03:02:08 +0400

    feat : added send logic

--- backend/shared/src/websocket/ws_manager.rs
+++ backend/shared/src/websocket/ws_manager.rs
@@ -1,13 +1,26 @@
-struct WebsocketManager{
-    websocket_url : String
+use tokio::sync::broadcast;
+use futures::{SinkExt, StreamExt};
+
+pub struct WebsocketManager{
+    sender : broadcast::Sender<String>
 }
 
 impl WebsocketManager {
-    fn new(ws_url : String) -> Self{
-        Self { websocket_url: ws_url }
+    pub fn new() -> Self{
+        let (sender, stream) = broadcast::channel(10);
+        Self { 
+            sender
+        }
     }
 
-    fn initialize_websocket(){
-        
+    pub fn send_message(&self, message : String){
+        match self.sender.send(message){
+            Ok(count) => {
+                println!("message sent to {} client", count)
+            }
+            Err(e) => {
+                println!("No clients connected to recive message : {}",e)
+            }
+        }
     }
 }
