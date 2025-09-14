341bdd3210357cf2f8f62218c102fe41c7038e8f
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-14 19:49:50 +0400

    feat : added websocket route

--- backend/api_server/src/main.rs
+++ backend/api_server/src/main.rs
@@ -1,4 +1,25 @@
+use axum::{extract::{ws::{WebSocket, WebSocketUpgrade}}, response::Response, routing::get, Router};
+use shared::websocket::ws_manager::WebsocketManager;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), anyhow::Error> {
+
+    println!("Starting API server...");
+
+    let ws_manager = WebsocketManager::new();
+
+    let api_routes  = Router::new()
+    .route("/", get(handler));
+
+    let ws_route = ws_manager.get_route();
+
+    let app = Router::new()
+    .nest("api", api_routes)
+    .merge(ws_route);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001").await?;
+    println!("Server runnning at port 3001");
+
+    axum::serve(listener,api_routes).await?;
+    Ok(())
 }
