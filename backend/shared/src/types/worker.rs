0319194081e38367afbf31024fefe667c2ed9f20
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-16 03:36:42 +0400

    fix : changed data types

--- backend/shared/src/types/worker.rs
+++ backend/shared/src/types/worker.rs
@@ -1,18 +1,19 @@
-use sea_orm::prelude::DateTimeLocal;
+use sea_orm::prelude::DateTimeUtc;
+use serde::Serialize;
 
-#[derive(Debug)]
-pub struct StructeredTransaction{
-    pub date : Time,
-    pub purchase_type : Type,
-    pub usd : Option<f64>,
-    pub token_quantity : f64,
-    pub token_price : f64,
-    pub owner : String,
-    pub dex_type : String,
-} 
+#[derive(Debug, Serialize)]
+pub struct StructeredTransaction {
+    pub date: DateTimeUtc,
+    pub purchase_type: Type,
+    pub usd: Option<f64>,
+    pub token_quantity: f64,
+    pub token_price: f64,
+    pub owner: String,
+    pub dex_type: String,
+}
 
-#[derive(Debug)]
-pub enum Type{
+#[derive(Debug, Serialize)]
+pub enum Type {
     Buy,
-    Sell
+    Sell,
 }
