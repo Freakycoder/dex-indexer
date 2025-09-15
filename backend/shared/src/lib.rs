77927539981aaf9b1e2891ac859fea73841e0951
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-16 03:42:31 +0400

    fix : removed program ids

--- backend/shared/src/lib.rs
+++ backend/shared/src/lib.rs
@@ -1,7 +1,4 @@
 pub mod redis;
 pub mod types;
-pub mod ys_grpc;
 pub mod websocket;
-
-const RADUIM_AMM_V4 : &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
-const RADUIM_CLMM : &str = "CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK";
+pub mod ys_grpc;
