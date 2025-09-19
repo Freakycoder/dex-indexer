9636e052383089ba22a5ec76f9682514da4c5dca
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-19 21:11:47 +0400

    feat : added grpc env var

--- backend/grpc_server/src/main.rs
+++ backend/grpc_server/src/main.rs
@@ -4,10 +4,12 @@ use shared::ys_grpc::client::GrpcClient;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     dotenv().ok();
-    let grpc_client = GrpcClient::new(std::env::var("GRPC_URL").expect("no grpc url in env"), std::env::var("GRPC_TOKEN").expect("no token in env"));
-    if let Err(e) = grpc_client.start_listening().await{
-        eprint!("grpc listener error : {}",e);
+    let grpc_client = GrpcClient::new(
+        std::env::var("GRPC_URL").expect("no grpc url in env"),
+        std::env::var("GRPC_TOKEN").expect("no token in env"),
+    );
+    if let Err(e) = grpc_client.start_listening().await {
+        eprint!("grpc listener error : {}", e);
     };
     Ok(())
 }
-
