8ba15826d07c7c54a9ace9929aba0a4f854efceb
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-20 11:23:01 +0400

    refactor: updated logging

--- backend/shared/src/redis/queue_manager.rs
+++ backend/shared/src/redis/queue_manager.rs
@@ -8,7 +8,7 @@ pub struct QueueManager {
 
 impl QueueManager {
     pub fn new() -> Result<Self, RedisError> {
-        println!("Initializing redis queue...");
+        println!("Initializing redis client...for QUEUE");
         let redis_url = "redis://localhost:6379";
         let redis_client = Client::open(redis_url).map_err(|e| {
             println!("Couldn't initialize a redis client : {}", e);
