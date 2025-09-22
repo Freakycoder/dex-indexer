81ea43c091299ae9316d1bc563e6cbee1eebeec9
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-23 03:26:08 +0400

    feat : integrated metrics redis storage

--- backend/shared/src/workers/metrics_worker.rs
+++ backend/shared/src/workers/metrics_worker.rs
@@ -1,17 +1,18 @@
 use std::time::Duration;
-
 use tokio::time::sleep;
-
-use crate::queues::structured_txn_manager::StructeredTxnQueueManager;
+use crate::{queues::structured_txn_manager::StructeredTxnQueueManager, redis::metric_and_ohlcv_manager::MetricOHLCVManager};
 
 #[derive(Debug)]
 pub struct MetricsWorker{
-    pub structured_txn_queue : StructeredTxnQueueManager
+    pub structured_txn_queue : StructeredTxnQueueManager,
+    pub metric_manager : MetricOHLCVManager
 }
 
 impl MetricsWorker {
-    pub fn new(structured_txn_queue : StructeredTxnQueueManager) -> Self{
-        Self { structured_txn_queue }
+    pub fn new(structured_txn_queue : StructeredTxnQueueManager) -> Result<Self, anyhow::Error>{
+
+        let metric_manager = MetricOHLCVManager::new()?;
+        Ok(Self { structured_txn_queue, metric_manager })
     }
 
     pub async fn start_processing(&self) {
@@ -21,6 +22,11 @@ impl MetricsWorker {
                 Ok(Some(txn_message)) => {
                     println!("Got txn messsage from the queue");
                     println!("Txn Metadata : {:?}", txn_message);
+                    let token_pair_clone = txn_message.token_pair.clone();
+                    self.metric_manager.update_current_price(txn_message.token_pair, txn_message.token_price).await;
+                    let market_cap = fastrand::i32(100_000..=1_000_000);
+                    let fdv = fastrand::i32(100_000..=1_000_000);
+                    self.metric_manager.update_market_data(token_pair_clone, market_cap, fdv).await;
                 }
                 Ok(None) => {
                     println!("Queue empty, no message recieved");
