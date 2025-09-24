78d75aa39c00344433781f2f6c5f1bba61fea5fd
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-24 20:54:05 +0400

    feat : added subcribe logic for websocket

--- backend/shared/src/redis/pubsub_manager.rs
+++ backend/shared/src/redis/pubsub_manager.rs
@@ -1,8 +1,15 @@
+use futures::StreamExt;
 use redis::{AsyncCommands, Client, RedisResult, RedisError};
+use tokio::sync::mpsc;
 use crate::{services::metrics_service::PeriodStatsUpdate, types::worker::StructeredTransaction};
 
-
 #[derive(Debug)]
+pub enum PubSubMessage{
+    Transaction(StructeredTransaction),
+    PriceMetrics(PeriodStatsUpdate)
+}
+
+#[derive(Debug, Clone)]
 pub struct PubSubManager {
     redis_client: Client,
 }
@@ -49,6 +56,70 @@ impl PubSubManager {
         conn.publish("price_metrics", message_json).await?;
         
         println!("📤 Published price and metrics update for {}", updated_data.token_pair);
+        Ok(())
+    }
+
+    // the websocket calls this fn.
+    pub async fn subscribe_to_channels(&self) -> RedisResult<mpsc::UnboundedReceiver<PubSubMessage>> {
+        let (tx, rx) = mpsc::unbounded_channel(); // we create unbounded mpsc channel to send messages to it through redis subscription
+        let client = self.redis_client.clone(); // created clone bcoz below code consumes the self (redis_client) 
+        tokio::spawn(async move {
+            if let Err(e) = Self::subscription_loop(client,tx).await { // an async task that starts at the side and pushes messages to mpsc
+                println!("PubSub subscription error: {}", e);
+            }
+        });
+        
+        Ok(rx) // webscoket of recieve the rx to consume message from mpsc
+    }
+
+    async fn subscription_loop(client : Client, tx : mpsc::UnboundedSender<PubSubMessage>) -> RedisResult<()>{
+        let mut pubsub = client.get_async_pubsub().await?;
+        pubsub.subscribe("transactions").await?; // sunscribe to both channels
+        pubsub.subscribe("price_metrics").await?;
+
+        println!("Subs to redis channel");
+        let mut pubsub_stream = pubsub.into_on_message();
+        while let Some(msg) = pubsub_stream.next().await{ // iterate of the stream to get messages
+            let channel = msg.get_channel_name();
+            let payload: String = match msg.get_payload() {
+                Ok(payload) => payload,
+                Err(e) => {
+                    println!("Failed to get payload from message: {}", e);
+                    continue;
+                }
+            };
+            match channel {
+                "transactions" => {
+                    match serde_json::from_str::<StructeredTransaction>(&payload) {
+                        Ok(txn) => {
+                            if let Err(_) = tx.send(PubSubMessage::Transaction(txn)){ // wrap the txn into an enum so it would be easy for websocket to identify the message                                println!("failed to send transaction to mpsc channel");
+                                break;
+                            }
+                        },
+                        Err(e) => {
+                            println!("Failed to desearilze transaction : {}",e);
+                        }
+                    }
+                }
+                "price_metrics" => {
+                    match serde_json::from_str::<PeriodStatsUpdate>(&payload) {
+                        Ok(stats) => {
+                            if let Err(_) = tx.send(PubSubMessage::PriceMetrics(stats)){
+                                println!("Failed to send update stats to mpsc channel");
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            println!("Failed to desearialize period stats : {}",e)
+                        }
+                    }
+                }
+                _ => {
+                    println!("⚠️ Received message from unknown channel: {}", channel);
+                }
+            }
+        };
+        println!("pubsub subscription loop ended");
         Ok(())
     }
 
