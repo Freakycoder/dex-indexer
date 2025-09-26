cf6aa19dc541ee59add4c5a38fa162de4760699c
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-09-26 12:07:13 +0400

    feat : added metrice_worker

--- backend/metrics_service/src/main.rs
+++ backend/metrics_service/src/main.rs
@@ -1,11 +1,17 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use shared::services::metrics_service::MetricsService;
+use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
     let metrics_service = Arc::new( MetricsService::new().expect("Error creating metrics service backgroud job"));
     println!("Starting the metrics service....");
-    metrics_service.start_all_schedulers().await
+    metrics_service.start_all_schedulers().await;
+    
+    // Keep the main thread alive indefinitely
+    loop {
+        sleep(Duration::from_secs(3600)).await;
+    }
 }
 
