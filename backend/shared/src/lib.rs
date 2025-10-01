30a171bc26509d88b0f3360a300a446f2ee120bf
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-10-02 01:35:58 +0400

    feat : added program ids

--- backend/shared/src/lib.rs
+++ backend/shared/src/lib.rs
@@ -6,3 +6,9 @@ pub mod entities;
 pub mod workers;
 pub mod queues;
 pub mod services;
+
+const RADUIM_AMM_V4: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+const RADUIM_CLMM: &str = "CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK";
+const METEORA_DLMM : &str = "LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo"; // named as Swap2
+const METEORA_DAMM_V2 : &str = "cpamdpZCGKUy5JxQXB4dcpGPiikHawvSWAd6mEn1sGG"; // equal to dyn2 & named as Swap
+const METEORA_DAMM_V1 : &str = "Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB"; // equal to dyn
