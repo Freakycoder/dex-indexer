ec0631617c5465343e693cc64680e39269035c1f
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-10-07 22:01:53 +0400

    fix : removed txn queue dependency

--- backend/txn_worker/src/main.rs
+++ backend/txn_worker/src/main.rs
@@ -1,4 +1,4 @@
-use shared::{queues::{structured_txn_manager::StructeredTxnQueueManager, swap_txn_manager::SwapTxnQueueManager}, workers::txn_worker::TxnWorker};
+use shared::{queues::{swap_txn_manager::SwapTxnQueueManager}, workers::txn_worker::TxnWorker};
 use dotenvy::dotenv;
 
 #[tokio::main]
@@ -7,8 +7,7 @@ async fn main() {
     println!(" Starting txn worker...");
 
     let swap_queue = SwapTxnQueueManager::new().expect("Failed to create swap queue");
-    let structured_txn_queue = StructeredTxnQueueManager::new().expect("Failed to create structured txn queue");
-    let worker = TxnWorker::new(swap_queue, structured_txn_queue);
+    let worker = TxnWorker::new(swap_queue);
 
     worker.start_processing().await;
 }
